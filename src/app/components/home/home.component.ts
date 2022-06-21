dc04bf6a4afebd046135747c10ea998faca2c9d7
Anvar Akhmadjanov <www.latvienhoasadrizeges@gmail.com>
2022-06-21 09:30:13 +0500

    created home component and routes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  sort = '';
+  games = [{
+    id: 1,
+    background_image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    name: 'Game 1',
+    parent_platforms: [{
+      platform:{
+        slug: 'pc',
+      }
+    }]
+  }];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  openGameDetails(id: number) {
+  }
+
+  searchGames(search: string) {
+  }
 }
