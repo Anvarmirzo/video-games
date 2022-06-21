bcdf9d821a826431e76f2a1666634467e355c9f7
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2022-06-22 01:49:21 +0500

    fixed prod vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,6 @@
-import {config} from 'dotenv';
-
-config();
-
 export const environment = {
   production: true,
-  BASE_URL: process.env['BASE_URL'],
-  API_KEY: process.env['API_KEY'],
-  API_HOST: process.env['API_HOST']
+  BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com',
+  API_KEY: '17c3eddb46msh29d177a4abcf49bp1b4fafjsnc4fb0df99ddf',
+  API_HOST: 'rawg-video-games-database.p.rapidapi.com'
 };
